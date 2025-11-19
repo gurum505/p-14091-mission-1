030be708056bd58ac4ca906f1c9890260c9817a1
gurum505 <sayhojeong505@gmail.com>
2025-11-19 15:32:14 +0900

    JSB 5

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    @ResponseBody
+    public String index(){
+        System.out.println("index");
+        return "메롱";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        System.out.println("hello");
+        return "hi";
+    }
+
+}
